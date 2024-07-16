45d80bc637be4c7c6ec6c709938a31e68282c424
jaimesHub <duongquockhanh230596@gmail.com>
2024-07-16 21:06:44 +0700

    default props & validate props

--- react-starter/02-props-and-list-rendering/practice/react-props/src/components/Product.js
+++ react-starter/02-props-and-list-rendering/practice/react-props/src/components/Product.js
@@ -1,35 +1,35 @@
-const Product = (props) => {
-    console.log(props.children);
+import PropTypes from 'prop-types';
+
+const Product = ({ name = "Default Props Value", type, price1, price2, isSale, isReview }) => {
     const renderPrice = () => {
-        switch (props.type) {
+        switch (type) {
             case 1:
-                return `$${props.price1} - $${props.price2}`;
+                return `$${price1} - $${price2}`;
             case 2:
                 return (
                     <>
-                        <span class="text-muted text-decoration-line-through">${props.price1}</span>
-                        ${props.price2}
+                        <span class="text-muted text-decoration-line-through">${price1}</span>
+                        ${price2}
                     </>
                 )
             default:
-                return `$${props.price1}`;
+                return `$${price1}`;
         }
     }
     return (
         <div className="col mb-5">
             <div className="card h-100">
-                {props.children}
                 {/* Sale badge*/}
-                {props?.isSale ? <div className="badge bg-dark text-white position-absolute" style={{ top: '0.5rem', right: '0.5rem' }}>Sale</div> : null}
+                {isSale ? <div className="badge bg-dark text-white position-absolute" style={{ top: '0.5rem', right: '0.5rem' }}>Sale</div> : null}
                 {/* Product image*/}
                 <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                 {/* Product details*/}
                 <div className="card-body p-4">
                     <div className="text-center">
                         {/* Product name*/}
-                        <h5 className="fw-bolder">{props.name}</h5>
+                        <h5 className="fw-bolder">{name}</h5>
                         {/* Product reviews*/}
-                        {props.isReview ? <div className="d-flex justify-content-center small text-warning mb-2">
+                        {isReview ? <div className="d-flex justify-content-center small text-warning mb-2">
                             <div className="bi-star-fill" />
                             <div className="bi-star-fill" />
                             <div className="bi-star-fill" />
@@ -48,5 +48,24 @@ const Product = (props) => {
         </div>
     );
 }
+
+// Validate Props
+Product.propTypes = {
+    name: PropTypes.string,
+    type: PropTypes.number,
+    price1: PropTypes.string,
+    price2: PropTypes.string,
+    isSale: PropTypes.bool,
+    isReview: PropTypes.bool
+}
+
+// Product.defaultProps = {
+//     name: "Default Props Value",
+//     type: 1,
+//     price1: "0.00",
+//     price2: "0.00",
+//     isSale: false,
+//     isReview: false
+// }
 
 export default Product;
--- react-starter/02-props-and-list-rendering/practice/react-props/src/layouts/Content.js
+++ react-starter/02-props-and-list-rendering/practice/react-props/src/layouts/Content.js
@@ -5,7 +5,7 @@ const Content = () => {
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <Product name="Fancy Product" type={1} price1="40.00" price2="80.00" >
+                    <Product type={1} price1="40.00" price2="80.00" >
                         <h1>Props children</h1>
                     </Product>
                     <Product name="Special Item" isSale={true} isReview={true} type={2} price1="40.00" price2="80.00" />
