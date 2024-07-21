0034be2c1e5332aefbf5f7460dadd47fff2c0e5d
jaimesHub <duongquockhanh230596@gmail.com>
2024-07-21 17:53:26 +0700

    applying useState()

--- react-starter/03-state-event/practice/state-and-events/src/App.js
+++ react-starter/03-state-event/practice/state-and-events/src/App.js
@@ -1,23 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const handleClickButton = (name) => {
-    console.log('You have clicked the HELLO button!');
-    alert(`Hello ${name}`);
-  }
-
-  // // tạo hàm mô phỏng 
-  // const handleClickElement = () => {
-  //   handleClickButton('James');
+  // const handleClickButton = (name) => {
+  //   console.log('You have clicked the HELLO button!');
+  //   alert(`Hello ${name}`);
   // }
 
-  const handleChangeInput = (event) => {
-    console.log(`You have changed value input: ${event.target.value}`)
+  // // // tạo hàm mô phỏng 
+  // // const handleClickElement = () => {
+  // //   handleClickButton('James');
+  // // }
+
+  // const handleChangeInput = (event) => {
+  //   console.log(`You have changed value input: ${event.target.value}`)
+  // }
+
+  // let number = 0;
+  const [number, setNumber] = useState(0);
+
+  // console.log(`Init number: ${number}`);
+
+  const handleAdd = () => {
+    // number += 1;
+    // console.log('Add: ', number)
+    setNumber(number + 1);
   }
+
+  const handleSubtract = () => {
+    // number -= 1;
+    // console.log('Subtract: ', number)
+    setNumber(number - 1);
+  }
+
 
   return (
     <div className="App">
-      <input onChange={handleChangeInput}></input>
+      {/* <input onChange={handleChangeInput}></input>
       <button style={{
         display: 'inline-block',
         width: '200px',
@@ -25,7 +44,22 @@ const App = () => {
         marginTop: '50px'
       }}
         onClick={() => handleClickButton('James')}
-      >Hello</button>
+      >Hello</button> */}
+
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+        <button style={{
+          display: 'inline-block',
+          height: '30px'
+        }} onClick={handleAdd}>Add(+)</button>
+        <h1>{number}</h1>
+        <button style={{
+          display: 'inline-block',
+          height: '30px'
+        }} onClick={handleSubtract}>Subtract(-)</button>
+      </div>
     </div>
   );
 }
