b0ee4490f298d782efc87882e4b340e22603b28b
jaimesHub <duongquockhanh230596@gmail.com>
2024-07-23 20:43:30 +0700

    Use navigate for redirect using useNavigate or isNavigate

--- react-starter/02-props-and-list-rendering/homeworks/clean-blog/src/components/Home.js
+++ react-starter/02-props-and-list-rendering/homeworks/clean-blog/src/components/Home.js
@@ -1,12 +1,28 @@
 import Footer from "../layouts/Footer";
 import Header from "../layouts/Header";
 import Navigation from "../layouts/Navigation";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
+
+    const [isRedirect, setIsRedirect] = useState(false);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        // setIsRedirect(true); // way 1
+        navigate('/about'); // way 2
+    }
+
+    if (isRedirect) {
+        return <Navigate to="/about" replace={isRedirect} />;
+    }
+
     return (
         <>
             <Navigation />
             <Header title="Home Page" />
+            <button onClick={handleClick}>Redirect</button>
             <Footer />
         </>
     );
