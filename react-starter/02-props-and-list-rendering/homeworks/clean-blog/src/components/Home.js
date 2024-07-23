3ea99007c48e2a98d0d39e6673c390272ea271aa
jaimesHub <duongquockhanh230596@gmail.com>
2024-07-23 20:56:38 +0700

    Search param implementation

--- react-starter/02-props-and-list-rendering/homeworks/clean-blog/src/components/Home.js
+++ react-starter/02-props-and-list-rendering/homeworks/clean-blog/src/components/Home.js
@@ -1,28 +1,35 @@
 import Footer from "../layouts/Footer";
 import Header from "../layouts/Header";
 import Navigation from "../layouts/Navigation";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
 import { useState } from "react";
 
 const Home = () => {
 
     const [isRedirect, setIsRedirect] = useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
+
     const navigate = useNavigate();
 
-    const handleClick = () => {
+    const handleRedirect = () => {
         // setIsRedirect(true); // way 1
-        navigate('/about'); // way 2
+        // navigate('/about'); // way 2
+
+        setSearchParams({ page: 5, limit: 10 });
     }
 
     if (isRedirect) {
         return <Navigate to="/about" replace={isRedirect} />;
     }
 
+    console.log(searchParams.get('page')); // http://localhost:3000/?page=5
+    console.log(searchParams.get('limit')); // http://localhost:3000/?limit=10
+
     return (
         <>
             <Navigation />
             <Header title="Home Page" />
-            <button onClick={handleClick}>Redirect</button>
+            <button onClick={handleRedirect}>Redirect</button>
             <Footer />
         </>
     );
