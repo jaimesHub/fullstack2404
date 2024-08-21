51623e4c9cf8f7454bcfa74f5f75dd651b538fcb
jaimesHub <duongquockhanh230596@gmail.com>
2024-08-21 20:04:24 +0700

    useQuery for state management

--- react-starter/10-react-query/example/src/App.js
+++ react-starter/10-react-query/example/src/App.js
@@ -9,8 +9,6 @@ function App() {
       </div>
       <Users />
     </>
-
-
   );
 }
 
--- react-starter/10-react-query/example/src/components/User.js
+++ react-starter/10-react-query/example/src/components/User.js
@@ -1,27 +1,22 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchUsers = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+}
 
 const Users = () => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['users'],
+        queryFn: fetchUsers,
+    });
 
-    const fetchUsers = async () => {
-        try {
-            const response = await fetch("https://jsonplaceholder.typicode.com/users");
-            const data = await response.json();
-            setUsers(data)
-        } catch (error) {
-            setError(error);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
-    if (loading) {
+    if (isLoading) {
         return <h1>Loading....</h1>
     }
 
@@ -29,15 +24,13 @@ const Users = () => {
         return <h1>Error: {error.message}</h1>;
     }
 
-    console.log(users)
-
     return (
         <ul>
-            {users.map((user) => (
+            {data?.map((user) => (
                 <li key={user.id}>{user.name}</li>
             ))}
         </ul>
-    )
+    );
 
 }
 
