587ad5b73930580ee1c6e96f072f7e7cdd1ef6c3
jaimesHub <duongquockhanh230596@gmail.com>
2024-10-08 21:59:06 +0700

    UPDATE: reset - init project

--- node-starter/03-express-advanced/src/index.ts
+++ node-starter/03-express-advanced/src/index.ts
@@ -3,24 +3,9 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
-
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, Express with TypeScript!');
 });
-
-// app.post('/send-email', (req: Request, res: Response) => {
-//     const { email } = req.body;
-
-//     emailQueue.add({ email });
-
-//     res.send("Job in queue");
-// });
-
-// emailQueue.process(async (job) => {
-//     const { email } = job.data;
-//     console.log(`Sending to email ${email}`)
-// });
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
