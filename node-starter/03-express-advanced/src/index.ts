56aadaba4f87435d3d6c08e0136c9b9a988a3a06
jaimesHub <duongquockhanh230596@gmail.com>
2024-10-08 22:16:37 +0700

    Handling JSON data

--- node-starter/03-express-advanced/src/index.ts
+++ node-starter/03-express-advanced/src/index.ts
@@ -3,8 +3,15 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json()); // Kích hoạt middleware để xử lý JSON 
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, Express with TypeScript!');
+});
+
+app.post('/api/data', (req: Request, res: Response) => {
+    console.log('>>> JSON data from client: ', req.body);
+    res.send('Data received!');
 });
 
 app.listen(PORT, () => {
