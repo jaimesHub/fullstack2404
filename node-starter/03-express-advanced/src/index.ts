e66ae93f8bab77512f823a070520b6bf70080ce8
jaimesHub <duongquockhanh230596@gmail.com>
2024-10-08 22:22:59 +0700

    Handling FORM data

--- node-starter/03-express-advanced/src/index.ts
+++ node-starter/03-express-advanced/src/index.ts
@@ -3,14 +3,20 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // Kích hoạt middleware để xử lý JSON 
+// app.use(express.json()); // Kích hoạt middleware để xử lý JSON 
+app.use(express.urlencoded({ extended: true })); // Kích hoạt middleware để xử lý dữ liệu FORM
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, Express with TypeScript!');
 });
 
-app.post('/api/data', (req: Request, res: Response) => {
-    console.log('>>> JSON data from client: ', req.body);
+// app.post('/api/data', (req: Request, res: Response) => {
+//     console.log('>>> JSON data from client: ', req.body);
+//     res.send('Data received!');
+// });
+
+app.post('/api/form', (req: Request, res: Response) => {
+    console.log('>>> FORM data from client: ', req.body);
     res.send('Data received!');
 });
 
