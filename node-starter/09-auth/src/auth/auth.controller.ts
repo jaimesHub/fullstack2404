f2ba921b9be4ca70870ef85887a2cfb16b0aafd6
jaimesHub <duongquockhanh230596@gmail.com>
2024-11-03 17:40:15 +0700

    Register controller

--- node-starter/09-auth/src/auth/auth.controller.ts
+++ node-starter/09-auth/src/auth/auth.controller.ts
@@ -1,7 +1,34 @@
 import { Request, Response } from 'express';
+import { User } from './auth.dto';
+import userModel from './auth.model';
+import bcrypt from 'bcrypt';
 
-export const authRegister = (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Register success' });
+export const authRegister = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const user: User = req.body;
+        const { email, password, username } = user;
+
+        const existingUser: User | null = await userModel.findOne({ email });
+
+        if (existingUser) {
+            res.status(404).json({ status: 'error', message: 'Email already exists' });
+            return;
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser: User = new userModel({
+            email,
+            password: hashedPassword,
+            username,
+            role: 'user',
+        });
+
+        res.status(201).json({ status: 'success', message: 'Register success' });
+
+    } catch (error: any) {
+        res.status(500).json({ status: 'error', message: error.message });
+    }
 }
 
 export const authLogin = (req: Request, res: Response) => {
--- node-starter/09-auth/src/auth/auth.model.ts
+++ node-starter/09-auth/src/auth/auth.model.ts
@@ -9,7 +9,7 @@ interface User extends Document {
 const UserSchema: Schema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     role: { type: String, required: true },
 }, { timestamps: true });
 
