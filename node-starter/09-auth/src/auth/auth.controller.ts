d59c6b02083b2ecdcaac4c8f686562185dd3e3c2
jaimesHub <duongquockhanh230596@gmail.com>
2024-11-03 18:10:57 +0700

    Login controller

--- node-starter/09-auth/src/auth/auth.controller.ts
+++ node-starter/09-auth/src/auth/auth.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 import { User } from './auth.dto';
 import userModel from './auth.model';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const authRegister = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -17,12 +21,14 @@ export const authRegister = async (req: Request, res: Response): Promise<void> =
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser: User = new userModel({
+        const newUser = new userModel({
             email,
             password: hashedPassword,
             username,
             role: 'user',
         });
+
+        await newUser.save();
 
         res.status(201).json({ status: 'success', message: 'Register success' });
 
@@ -31,6 +37,38 @@ export const authRegister = async (req: Request, res: Response): Promise<void> =
     }
 }
 
-export const authLogin = (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Login success' });
+export const authLogin = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const payload: User = req.body;
+        const { email, password } = payload;
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            res.status(404).json({
+                status: 'error',
+                message: 'Email is not correct'
+            });
+            return;
+        }
+
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(404).json({
+                status: 'error',
+                message: 'Password is not correct'
+            });
+            return;
+        }
+
+        const SECRET_KEY: string = process.env.JWT_SECRET || 'abcdefghijklmnopqrstuvwxyz';
+        const token = jwt.sign({ email: user.email, userId: user._id }, SECRET_KEY, { expiresIn: '1h' });
+
+        res.status(200).json({ status: 'success', message: 'Login success', token });
+
+        return;
+
+    } catch (error: any) {
+        res.status(500).json({ status: 'error', message: error.message });
+    }
 }
